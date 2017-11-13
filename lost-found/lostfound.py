0bcad96ddfe167ed70269b3a24cb40773123a7c0
Kai <kaichenusesthis@gmail.com>
2017-11-13 10:58:06 -0800

    Connects to database

--- lost-found/lostfound.py
+++ lost-found/lostfound.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-
+# 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:opensesame@localhost/postgres'
+db = SQLAlchemy(app)
 
 # Blueprints
 from views.public import public_routes
