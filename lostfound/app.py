dd6a1abc4e87931ce4ae194f652825f23e01128d
Kai Chen <kaichenusesthis@gmail.com>
2017-11-16 01:52:10 +0000

    changes for deploy

--- lostfound/app.py
+++ lostfound/app.py
@@ -8,12 +8,12 @@ from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://kai:opensesame@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
 # psql -h localhost -U kai postgres
 # flask run -h 0.0.0.0 -p 8080
 
-#'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b
-# 58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
+# 'postgresql://kai:opensesame@localhost:5432/postgres'
+#'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 db.init_app(app)
 
