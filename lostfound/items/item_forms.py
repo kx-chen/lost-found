5b71a380bd2128321a2fa039c6a51bbb23f1daff
Kai Chen <kaichenusesthis@gmail.com>
2017-11-20 05:53:23 +0000

    Changed flask_wtf import to get rid of warning

--- lostfound/items/item_forms.py
+++ lostfound/items/item_forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField, SubmitField, validators, TextAreaField
 
 class NewItemForm(Form):
--- lostfound/users/forms.py
+++ lostfound/users/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField, SubmitField, validators
 
 class SignupForm(Form):
