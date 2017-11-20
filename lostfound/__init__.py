ff45a5cba38c52305cce44486b7a628fd45d0f01
Kai Chen <kaichenusesthis@gmail.com>
2017-11-20 03:23:46 +0000

    App instance is now in __init__.py

--- lostfound/__init__.py
+++ lostfound/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
-from appConfig.dbClient import db
-from appConfig.appFactory import create_app
+from lostfound.appConfig.dbClient import db
+from lostfound.appConfig.appFactory import create_app
 
 app = create_app()
-app.config['SQLALCHEMY_DATABASE_URI'] =  'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
+
 # psql -h localhost -U kai postgres
 # heroku pg:psql postgresql-animated-12554 --app lostfoundapp
 # flask run -h 0.0.0.0 -p 8080
 
 # 'postgresql://kai:opensesame@localhost:5432/postgres'
 #'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-db.init_app(app)
+
+
 
 
 # Blueprints
--- lostfound/appConfig/appFactory.py
+++ lostfound/appConfig/appFactory.py
@@ -3,5 +3,9 @@ from .dbClient import db
 
 
 def create_app(cfg=None):
-    app = Flask(__name__)
+    app = Flask('lostfound')
+    app.config['SQLALCHEMY_DATABASE_URI'] =  'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    
+    db.init_app(app)
     return app
