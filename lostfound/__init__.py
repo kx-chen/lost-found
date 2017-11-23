ddd4759d053833566352d30a778e8b14e42c2946
Kai Chen <kaichenusesthis@gmail.com>
2017-11-23 05:44:50 +0000

    Removed comments

--- lostfound/__init__.py
+++ lostfound/__init__.py
@@ -2,12 +2,3 @@ from flask import Flask
 from lostfound.appConfig.appFactory import create_app
 
 app = create_app()
-
-
-# psql -h localhost -U kai postgres
-# heroku pg:psql postgresql-animated-12554 --app lostfoundapp
-# export FLASK_APP=lostfound:app
-# flask run -h 0.0.0.0 -p 8080
-
-# 'postgresql://kai:opensesame@localhost:5432/postgres'
-#'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
