be1712dae3826a5bfc46a76351e26c900bb64a50
Kai Chen <kaichen120@gmail.com>
2017-12-21 02:21:31 +0000

    tests: Modify coveralls and coverage config files .

---
+++ lostfound/config.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://kai:opensesame@localhost:5432/postgres'
+
+# psql -h localhost -U kai postgres
+# heroku pg:psql postgresql-animated-12554 --app lostfoundapp
+# export FLASK_APP=lostfound:app
+# flask run -h 0.0.0.0 -p 8080
+#coverage run runTest.py
+
+# 'postgresql://kai:opensesame@localhost:5432/postgres'
+#'postgres://vxtsfazasjzswx:343e88d24e1d42804c275bcfc1dc9b58cc294ce0c47870bac43a25ca87fe8ca1@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d8l24k56ov6qsv'
